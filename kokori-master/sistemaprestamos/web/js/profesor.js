849d873b6af6f21c0897974f3e98d4445740d723
diegopuma12a <38229865+diegopuma12a@users.noreply.github.com>
2018-07-05 15:37:38 -0500

    ste kevin

--- kokori-master/sistemaprestamos/web/js/profesor.js
+++ kokori-master/sistemaprestamos/web/js/profesor.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
-<<<<<<< HEAD
-=======
+
     //$('.collapsible').collapsible();
     
               console.log("funciona: JQUERY");
           $('select').formSelect();
     
->>>>>>> fa4511aa91352ef2b7ac2554cf3056d8a6b55f15
+
     
 });
 /**
