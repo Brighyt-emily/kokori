aad8bb85ea0e9f10c45edeb0ba75bc5cd045c818
Brighyt-emily <Brighyt-emily@users.noreply.github.com>
2018-07-05 16:42:43 -0500

    prod

---
+++ kokori-master/sistemaprestamos/src/java/pe/edu/upeu/presup/dao/DevolucionDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.upeu.presup.dao;
+
+import java.util.List;
+import pe.edu.upeu.presup.entity.Devolucion;
+
+/**
+ *
+ * @author EMILY
+ */
+public interface DevolucionDao {
+    int delete (int key);
+    List<Devolucion> readAll();
+}
