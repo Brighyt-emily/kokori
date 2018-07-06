aa5dee8c0ba675535978ebf48da82a9951f9b438
Brighyt-emily <Brighyt-emily@users.noreply.github.com>
2018-07-06 12:25:47 -0500

    tip

---
+++ kokori-master/sistemaprestamos/src/java/pe/edu/upeu/presup/entity/Tipo.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.upeu.presup.entity;
+
+/**
+ *
+ * @author EMILY
+ */
+public class Tipo {
+    private int id;
+    private String nom;
+
+    public Tipo() {
+    }
+
+    public Tipo(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public Tipo(String nom) {
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
+    
+    
+}
