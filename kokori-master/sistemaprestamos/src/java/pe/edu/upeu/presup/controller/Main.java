ee7c0805518cb64ce1b33f773713fa954a18a7eb
Marco Bernabé Chunga Oqueña <marcochunga@upeu.edu.pe>
2018-07-08 17:05:48 -0500

    0.1.1

---
+++ kokori-master/sistemaprestamos/src/java/pe/edu/upeu/presup/controller/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.upeu.presup.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import pe.edu.upeu.presup.dao.UsuarioDao;
+import pe.edu.upeu.presup.daoimp.UsuarioDaoImp;
+
+/**
+ *
+ * @author Marco
+ */
+public class Main extends HttpServlet{
+    private UsuarioDao ud = new UsuarioDaoImp();
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        int op = Integer.parseInt(request.getParameter("op"));
+        HttpSession sesion = request.getSession();
+        RequestDispatcher rd;
+        ServletContext context= getServletContext();
+        HashMap<String, Object> datos = new HashMap<>();
+    }
+}
