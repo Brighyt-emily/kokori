2ab4366f33ce53e2f49f70e5c090e47a54228d14
Brighyt-emily <Brighyt-emily@users.noreply.github.com>
2018-07-08 00:58:35 -0500

    holi

--- kokori-master/sistemaprestamos/web/js/producto.js
+++ kokori-master/sistemaprestamos/web/js/producto.js
@@ -11,8 +11,14 @@ function listar()
         $("#tablin tbody tr").remove();
         for (var i = 0; i < data.length; i++) {
             $("#tablin").append("<tr><td>"+(i+1)+"</td><td>" + bibi[i].nom + "</td><td>" + bibi[i].cod + "</td><td>" 
-                    + bibi[i].est + "</td><td>" + bibi[i].itip + "</td><td></td></tr>");
+                    + bibi[i].est + "</td><td>" + bibi[i].iTip + "</td><td></td></tr>");
         }
+    });
+}
+function eliminar(x)
+{
+    $.get("pro",{"idpro":x,"opc":3},function () {
+        listarCatalogo();
     });
 }
         
