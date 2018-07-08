b45409e60244544e97361e1f6a9c0db3b627db28
Marco Bernabé Chunga Oqueña <marcochunga@upeu.edu.pe>
2018-07-08 17:48:17 -0500

    0.1.4

--- kokori-master/sistemaprestamos/src/java/pe/edu/upeu/presup/controller/Main.java
+++ kokori-master/sistemaprestamos/src/java/pe/edu/upeu/presup/controller/Main.java
@@ -22,9 +22,10 @@ import pe.edu.upeu.presup.daoimp.UsuarioDaoImp;
  *
  * @author Marco
  */
-public class Main extends HttpServlet{
+public class Main extends HttpServlet {
+
     private UsuarioDao ud = new UsuarioDaoImp();
-    
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -32,9 +33,26 @@ public class Main extends HttpServlet{
         int op = Integer.parseInt(request.getParameter("op"));
         HttpSession sesion = request.getSession();
         RequestDispatcher rd;
-        ServletContext context= getServletContext();
+        ServletContext context = getServletContext();
         HashMap<String, Object> datos = new HashMap<>();
         datos = ud.validar(request.getParameter("user"), request.getParameter("pass"));
-        
+        switch (op) {
+            case 1:
+                if (datos.size() > 0) {
+                    sesion.setAttribute("iduser", datos.get("idu"));
+                    sesion.setAttribute("user", datos.get("user"));
+                    String nombres = datos.get("nom") + " " + datos.get("apell");
+                    sesion.setAttribute("nombres", nombres);
+                    sesion.setAttribute("rol", datos.get("rol"));
+                    sesion.setAttribute("idr", datos.get("irol"));
+                    sesion.setAttribute("idt", datos.get("itra"));
+                    System.out.println(datos);
+                    rd = request.getRequestDispatcher("/menu");
+                    rd.forward(request, response);
+                } else {
+                    rd = request.getRequestDispatcher("/login");
+                    rd.forward(request, response);
+                }
+        }
     }
 }
