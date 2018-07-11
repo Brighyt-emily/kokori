486dff1046fbd92a05352ef044713855d339dee0
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2018-07-10 19:08:47 -0500

    dfghop

--- kokori-master/sistemaprestamos/web/js/registrosReservas.js
+++ kokori-master/sistemaprestamos/web/js/registrosReservas.js
@@ -1,11 +1,13 @@
  $(document).ready(function () {
     listarRegistroReservas();
+    alert("Hola");
 });
 
 function listarRegistroReservas() {
     $("#tblRegistro tbody tr").remove();
     $.post("rc", {"op": 6}, function (data) {
         var x = JSON.parse(data);
+        alert(data);
         for (var i = 0; i < x.length; i++) {
             $("#tblRegistro").append("<tr><td>" + x[i].feReserva + "</td><td>" + x[i].nomProfesor + " " + x[i].aplProfesor + "</td><td>" + x[i].codigo + "</td><td>" + x[i].dni + "</td><td><a href='#' onclick='editarReserva(" + x[i].idReserva + ")'><i class='material-icons'>mode_edit</i></a></td><td><a href = '#' onclick='eliminarReserva(" + x[i].idReserva + ")'><i class='material-icons'>delete_sweep</i></a></td></tr>");
         }
@@ -19,7 +21,7 @@ function editarReserva(idr) {
     alert("editar: " + idr);
 }
 
-/**$("#filtNom").keyup(function () {
+$("#filtNom").keyup(function () {
     var tableReg = document.getElementById('tblRegistro');
     var searchText = document.getElementById('filtNom').value.toLowerCase();
     for (var i = 1; i < tableReg.rows.length; i++) {
@@ -37,4 +39,4 @@ function editarReserva(idr) {
             tableReg.rows[i].style.display = 'none';
         }
     }
-});*/
+});
