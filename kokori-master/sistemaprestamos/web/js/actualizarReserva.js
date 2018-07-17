a25d86cb2af0064f8b33f268319bd9542d476c48
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2018-07-17 13:43:33 -0500

    quebin-final-rios

--- kokori-master/sistemaprestamos/web/js/actualizarReserva.js
+++ kokori-master/sistemaprestamos/web/js/actualizarReserva.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
 
     DatosByReserva();
-    //listarProducto();
-    //mostrarProdByReserva();
 });
 
 var arregloReserva = new Array();// arreglo para guardar los datos de la reserva
@@ -10,6 +8,27 @@ var arregloReserva = new Array();// arreglo para guardar los datos de la reserva
 var arregloProductoByIdDr = new Array();// arreglo para guardar los iddr(id de detalle reserva)
 
 var productosReserva = new Array();//arreglo para guardar productos de la reserva-- productos antiguos
+
+
+$("#nomProducto").keyup(function () {
+    var tableReg = document.getElementById('actuProd');
+    var searchText = document.getElementById('nomProducto').value.toLowerCase();
+    for (var i = 1; i < tableReg.rows.length; i++) {
+        var cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
+        var encontrado = false;
+        for (var j = 0; j < cellsOfRow.length && !encontrado; j++) {
+            var compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+            if (searchText.length === 0 || (compareWith.indexOf(searchText) > -1)) {
+                encontrado = true;
+            }
+        }
+        if (encontrado) {
+            tableReg.rows[i].style.display = '';
+        } else {
+            tableReg.rows[i].style.display = 'none';
+        }
+    }
+});
 
 function DatosByReserva() {
     $("#actuProd tbody tr").remove();
@@ -22,6 +41,7 @@ function DatosByReserva() {
                     + "<a href='#' onclick='productoSeleccionado(" + w[i].idProducto + ")'>"
                     + "<i class = 'material-icons prefix'>check_circle</i></a></td</tr>");
         }
+
         //enviamos los datos de la reserva
         $("#reservado tbody tr").remove();// limpiamos la tabla 
         var id = $("#idresquebin").val();
@@ -54,37 +74,52 @@ function datosProductos() {
         obj.idp = $(this).find("td").eq(0).html();
         obj.nombre = $(this).find("td").eq(1).html();
         obj.codigo = $(this).find("td").eq(2).html();
-        if (productosReserva.length > 0) {
-            var j = 0;
-            while (j < productosReserva.length) {
-                if (productosReserva[j]) {
-
-                }
-            }
-        }
+        productosReserva.push(obj);
     });
 }
 
-$("#nomProducto").keyup(function () {
-    var tableReg = document.getElementById('actuProd');
-    var searchText = document.getElementById('nomProducto').value.toLowerCase();
-    for (var i = 1; i < tableReg.rows.length; i++) {
-        var cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
-        var encontrado = false;
-        for (var j = 0; j < cellsOfRow.length && !encontrado; j++) {
-            var compareWith = cellsOfRow[j].innerHTML.toLowerCase();
-            if (searchText.length === 0 || (compareWith.indexOf(searchText) > -1)) {
-                encontrado = true;
-            }
-        }
-        if (encontrado) {
-            tableReg.rows[i].style.display = '';
-        } else {
-            tableReg.rows[i].style.display = 'none';
-        }
-    }
-});
+function productoSeleccionado(w) {
+    $.get("rc", {"op": 3, "idProducto": w}, function (data) {
 
-function productoSeleccionado(q){
-    alert(q);
+        var x = JSON.parse(data);
+        var obj = new Object();
+        obj.idp = x[0].idproducto;
+        obj.nombre = x[0].nomProducto;
+        obj.codigo = x[0].codigo;
+        añadirListado(obj);
+        listarProdReservados();
+    });
+}
+function añadirListado(objeto) {
+    if (productosReserva.length > 0) {
+        var j = 0;
+        while (j < productosReserva.length) {
+            if (productosReserva[j].codigo === objeto.codigo) {
+                var toastContent = $('<span class="white-text"><b>Producto en lista!!</b></span>');
+                Materialize.toast(toastContent, 1800);
+                j = productosReserva.length;
+                objeto = null;
+            }
+            j++;
+        }
+        if (objeto !== null) {
+            productosReserva.push(objeto);
+        }
+    } else {
+        productosReserva.push(objeto);
+    }
+}
+
+function listarProdReservados() {
+    $("#reservado tbody tr").remove();
+    for (var i = 0; i < productosReserva.length; i++) {
+        $("#reservado").append("<tr><td>" + productosReserva[i].idp
+                + "</td><td>" + productosReserva[i].nombre + "</td><td>"
+                + productosReserva[i].codigo
+                + "</td><td><a href ='#' onclick='eliminar(" + i + ");'><i class='material-icons'>delete_sweep</i></a></td></tr>");
+    }
+}
+
+function eliminar(i) {
+    alert(i);
 }
