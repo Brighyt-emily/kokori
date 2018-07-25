26b383fceb0ad2ddc5e5a1a859dc54fbf1a96eee
diegopuma12a <38229865+diegopuma12a@users.noreply.github.com>
2018-07-25 11:30:29 -0500

    ahora?

--- kokori-master/sistemaprestamos/web/InformProfesor.jsp
+++ kokori-master/sistemaprestamos/web/InformProfesor.jsp
@@ -12,11 +12,7 @@
         <title>JSP Page</title>
          <%@include file="WEB-INF/template/header.jspf" %>
 
-         <!-- Compiled and minified CSS -->
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
-
-         <!-- Compiled and minified JavaScript -->
-         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+        
 
     </head>
     <body>
--- kokori-master/sistemaprestamos/web/js/verprofesor.js
+++ kokori-master/sistemaprestamos/web/js/verprofesor.js
@@ -27,23 +27,10 @@ function cardVer(){
         $("#txtgrado").val(w[0].grado);
         $("#txtescuela").val(w[0].nomEscuela);
         $("#txtdireccion").val(w[0].direccion);
+        actualizapro();
     });
     
-    
-    $.get("pc", {"idProf": idpars, "op": 8}, function (datas) {
-        alert(datas);
-        var w = JSON.parse(datas);
-        alert("estos es la data" + w);
-        alert(w[0].fechaP);
-        console.log("llego a controlador");
-        for (var i = 0; i < w.length; i++) {
-            $("#tblprestamos").append("<tr><td hidden>" + w[i].idprestamo + "</td><td>"
-                    + w[i].fechaP + "</td><td>" + w[i].fechaD + "</td><td>"
-                    + w[i].horaP + "</td><td>" + w[i].horaD + "</td><td>" + w[i].aula + "</td><td><a href='#' onclick='verproductos(" + w[i].idprestamo + ")'>"
-                    + "<i class = 'material-icons prefix'>check_circle</i></a></td</tr>");
-        }
-
-    });
+   
 }
 
 $("#btnMostrar").click(function () {
@@ -68,14 +55,25 @@ $("#btnMostrar").click(function () {
 
 
 function actualizapro(){
-    
+        var id = $("#idP").val();
+   var idpars = parseInt(id);
+    $.get("pc", {"idProf": idpars, "op": 8}, function (datas) {
+        alert(datas);
+        var w = JSON.parse(datas);
+        alert("estos es la data" + w);
+        alert(w[0].fechaP);
+        console.log("llego a controlador");
+        for (var i = 0; i < w.length; i++) {
+            $("#tblprestamos").append("<tr><td hidden>" + w[i].idprestamo + "</td><td>"
+                    + w[i].fechaP + "</td><td>" + w[i].fechaD + "</td><td>"
+                    + w[i].horaP + "</td><td>" + w[i].horaD + "</td><td>" + w[i].aula + "</td><td><a href='#' onclick='verproductos(" + w[i].idprestamo + ")'>"
+                    + "<i class = 'material-icons prefix'>check_circle</i></a></td</tr>");
+        }
+
+    });
 }
 
-function listarprestaprof(){
-    
-   
-    
-}
+
 
 
 function verproductos(x){
